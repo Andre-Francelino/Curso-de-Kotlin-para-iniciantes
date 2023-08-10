8e29910fcec9548ce9d4a78c16fd7efc73cd703e
Andre Francelino <andrefgomes.github.version@gmail.com>
2023-08-09 22:35:49 -0300

    Aula01 - variáveis mutáveis e imutáveis

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    
+}
+rootProject.name = "curso-kotlin-para-iniciantes"
+
---
+++ src/main/kotlin/mainAula01.kt
@@ -0,0 +1,15 @@
+// Aula 01 - Variáveis mutáveis e imutáveis
+
+fun main(args: Array<String>){
+    println("Olá mundo! Bem vindo ao curso de Kotlin para iniciantes.")
+
+    // Variável Mutável - são iniciadas por "var"
+    var nome = "André Gomes"
+    nome = "André Francelino"
+    println(nome)
+
+    // Variável Imutável - são iniciadas por "val"
+    val idade = 31
+    println(idade)
+
+}
