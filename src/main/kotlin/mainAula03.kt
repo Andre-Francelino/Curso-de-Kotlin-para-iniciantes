cdb8307196addd543b2d8cba4776483f6e2205d0
Andre Francelino <andrefgomes.github.version@gmail.com>
2023-08-12 06:53:46 -0300

    Aula03 - Concatenação

---
+++ src/main/kotlin/mainAula03.kt
@@ -0,0 +1,12 @@
+// Aula 03 - Concatenação
+
+fun main(args: Array<String>) {
+
+    var nome = "André"
+    var idade = 31
+    var profissao = "Desenvolvedor Android"
+
+    //println("Meu nome é " + nome + ", tenho " + idade + " anos e sou " + profissao)
+    println("Meu nome é $nome, tenho $idade anos e sou $profissao")
+
+}
