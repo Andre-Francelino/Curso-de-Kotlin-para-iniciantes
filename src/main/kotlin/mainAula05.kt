9ba7bf8debf8ee28798c3144a741fabbd9b97605
Andre Francelino <andrefgomes.github.version@gmail.com>
2023-08-14 12:31:00 -0300

    Aula05 - operadores relacionais

---
+++ src/main/kotlin/mainAula05.kt
@@ -0,0 +1,23 @@
+// Aula 05 - Operadores Relacionais
+
+fun main(args: Array<String>) {
+    /*
+        == igual
+        < menor que
+        > maior que
+        <= menor ou igual
+        >= maior ou igual
+        != diferente de
+    */
+
+    var valor1 = 20
+    var valor2 = 10
+
+    println(valor1 == valor2)
+    println(valor1 < valor2)
+    println(valor1 > valor2)
+    println(valor1 <= valor2)
+    println(valor1 >= valor2)
+    println(valor1 != valor2)
+
+}
