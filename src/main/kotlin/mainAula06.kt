566f1cd95f6b6ba227792be3b73c23cb99b784a0
Andre Francelino <andrefgomes.github.version@gmail.com>
2023-08-15 06:51:46 -0300

    Aula06 - Estrutura condicional If/Else

---
+++ src/main/kotlin/mainAula06.kt
@@ -0,0 +1,14 @@
+// Aula 06 - Estrutura Condicional If/Else
+
+fun main(args: Array<String>) {
+
+    var idade = 19
+
+    // Se (if) Senão (else)
+    if(idade >= 18) {
+        println("O usuário é maior de idade.")
+    } else {
+        println("O usuário é menor de idade.")
+    }
+
+}
---
+++ src/main/kotlin/mainAula06_2.kt
@@ -0,0 +1,15 @@
+// Aula 06 - Estrutura Condicional If/Else/Else-If
+
+fun main(args: Array<String>) {
+
+    var idade = 100
+
+    if (idade <= 10) {
+        println("Criança")
+    } else if (idade <= 17) {
+        println("Adolescente")
+    } else {
+        println("Adulto")
+    }
+
+}
