52fc61f37c1d411f362563b4f7c53c59df5ed92b
Andre Francelino <andrefgomes.github.version@gmail.com>
2023-08-16 07:07:48 -0300

    Aula 07 - Operadores Lógicos

---
+++ src/main/kotlin/mainAula07.kt
@@ -0,0 +1,13 @@
+// Aula 07 - Operadores Lógicos
+
+fun main(args: Array<String>) {
+
+    // Operador && - E/AND: É verdadeiro quando todas as premissas são verdadeiras
+
+    if (20 == 20 && 10 < 20 && 50 > 30) {
+        println("Verdadeiro")
+    } else {
+        println("Falso")
+    }
+
+}
---
+++ src/main/kotlin/mainAula07_2.kt
@@ -0,0 +1,12 @@
+// Aula 07 - Operadores Lógicos
+
+fun main(args: Array<String>) {
+
+    // Operador || - OU/OR: É verdadeiro quando pelo menos uma das premissas é verdadeira
+
+    if (20 == 20 || 21 < 20) {
+        println("Verdadeiro")
+    } else {
+        println("Falso")
+    }
+}
